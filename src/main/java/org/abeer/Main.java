e569aa5e303cc1801be0c6c67d66dfb0c8ca1a62
abeerrai01 <theabeerrai.gmail.com>
2025-06-06 05:51:24 +0530

    BEAN LIFECYCLE IMPLEMENTED

--- src/main/java/org/abeer/Main.java
+++ src/main/java/org/abeer/Main.java
@@ -1,6 +1,7 @@
 package org.abeer;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
@@ -8,18 +9,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
-        Ram ram=context.getBean("Ram",org.abeer.Ram.class);
-        Shyam shyam=context.getBean("Shyam",org.abeer.Shyam.class);
-        Address address=context.getBean("Address",org.abeer.Address.class);
-        System.out.println(ram);
-        ram.display();
-        System.out.println(shyam);
-        shyam.display();
-        System.out.println(address);
-        ram.eat();
-        ram.sleep();
-        shyam.eat();
-        shyam.sleep();
+        //ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        //Ram ram=context.getBean("Ram",org.abeer.Ram.class);
+        //Shyam shyam=context.getBean("Shyam",org.abeer.Shyam.class);
+        //Address address=context.getBean("Address",org.abeer.Address.class);
+        //System.out.println(ram);
+        //ram.display();
+        //System.out.println(shyam);
+        //shyam.display();
+        //System.out.println(address);
+        //ram.eat();
+        //ram.sleep();
+        //shyam.eat();
+        //shyam.sleep();
+
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        context.registerShutdownHook();
+
+        Student student=context.getBean("st",org.abeer.Student.class);
+        System.out.println(student);
     }
 }
---
+++ src/main/java/org/abeer/Student.java
@@ -0,0 +1,40 @@
+package org.abeer;
+
+public class Student
+{
+    private int id;
+    private String name;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    public void init()
+    {
+        System.out.println("INITIALIZING METHOD");
+    }
+    public void destroy()
+    {
+        System.out.println("DESTROYING METHOD");
+    }
+
+
+}
